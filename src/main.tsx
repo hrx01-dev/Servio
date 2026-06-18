db524340decd773f2f8dead800c65670873907a9
Jayam Srivastava <jayamsrivastava99@gmail.com>
2026-06-18 23:36:40 +0530

    fixed tsx import extension breaks standard tsc; missing skip-link / section landmarks

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 
   import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
+import App from "./app/App";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <>
+    <a href="#main-content" className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 focus:z-[100] focus:p-4 focus:bg-white focus:text-indigo-600 focus:rounded-lg focus:shadow-xl">Skip to content</a>
+    <App />
+  </>
+);
